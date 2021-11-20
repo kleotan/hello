39cfcf23696179174a8e030206d274e15751ba74
kleotan <tanshi901@gmail.com>
2021-11-20 16:36:12 +0200

    changes input

--- newClass.py
+++ newClass.py
@@ -1,8 +1,7 @@
 class Point ():
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-        
+    def __init__(self, input1, input2):
+        self.x = input1
+        self.y = input2
 
 p = Point (15, 8)
 print(p.x)
